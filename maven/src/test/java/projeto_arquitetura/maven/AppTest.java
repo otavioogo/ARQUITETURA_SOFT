5f0342f9994325e4adf1c75359e34d068fe93c24
otavioogo <79700599+otavioogo@users.noreply.github.com>
2021-03-04 00:05:24 -0300

    Update AppTest.java

--- maven/src/test/java/projeto_arquitetura/maven/AppTest.java
+++ maven/src/test/java/projeto_arquitetura/maven/AppTest.java
@@ -1,13 +1,40 @@
-package projeto_arquitetura.maven;
+package Solo;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
-
-public class AppTest {
-
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
-
+public class Solos {
+	
+//	podia fazer um if e depois retornar , mas preferir o mais simples.
+	
+	
+	private Element soloIdeal(int tipoSolo) {
+		Element elemento = new Element();
+		switch (tipoSolo) {
+			case 1:
+				System.out.println("Argiloso");
+                                
+				elemento.setFosforo(9.0);
+				elemento.setPotassio(0.35);
+				elemento.setCalcio(6.0);
+				elemento.setMagnesio(1.5);
+				elemento.setEnxofre(9.0);
+				elemento.setAluminio(0);
+				elemento.sethAl(0);
+				return elemento;
+			case 2:
+				System.out.println("Solo Text. Média");
+                                
+				elemento.setFosforo(12.0);
+				elemento.setPotassio(0.25);
+				elemento.setCalcio(6.0);
+				elemento.setMagnesio(1.5);
+				elemento.setEnxofre(9.0);
+				elemento.setAluminio(0);
+				elemento.sethAl(0);
+				return elemento;
+			default:
+                            
+                            
+				System.out.println("Solo não encontrando!");
+				return elemento;
+		}
+	}
 }
